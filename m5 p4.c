0c1b33565bfef69561e4b9e7759feccf8fdd386f
girishdev s <girishdev66@gmail.com>
2026-01-16 18:19:02 +0530

    Add files via upload

---
+++ m5 p4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main() {
+    findLargest(int a, int b, int c) {
+    if (a >= b && a >= c) {
+        return a;
+    } else if (b >= a && b >= c) {
+        return b;
+    } else {
+        return c;
+    }
+}
