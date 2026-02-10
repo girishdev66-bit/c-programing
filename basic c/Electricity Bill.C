f61dad4ffb9930bc2fe62f972d98c18b8adb421b
girishdev s <girishdev66@gmail.com>
2026-02-10 19:02:47 +0530

    Create Electricity Bill.C

---
+++ basic c/Electricity Bill.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int units;
+    float bill;
+    scanf("%d", &units);
+    if (units <= 50)
+        bill = units * 3;
+    else if (units <= 100)
+        bill = 50 * 3 + (units - 50) * 4;
+    else
+        bill = 50 * 3 + 50 * 4 + (units - 100) * 5;
+    printf("Bill amount = Rs. %.2f", bill);
+    return 0;
+}
