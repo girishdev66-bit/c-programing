93a78d39015cf6570d23002c6dfae07aea386e16
girishdev s <girishdev66@gmail.com>
2026-03-03 16:33:42 +0530

    Create Even–Odd Balanced Departments.C

---
+++ basic c/2D ARRAY/Even–Odd Balanced Departments.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int R,C;
+    scanf("%d%d",&R,&C);
+    int a[R][C];
+    int count=0;
+
+    for(int i=0;i<R;i++){
+        int even=0,odd=0;
+        for(int j=0;j<C;j++){
+            scanf("%d",&a[i][j]);
+            if(a[i][j]%2==0) even++;
+            else odd++;
+        }
+        if(even==odd) count++;
+    }
+
+    printf("%d",count);
+    return 0;
+}
