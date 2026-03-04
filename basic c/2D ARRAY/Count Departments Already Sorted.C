85e4ae522fd96da44ea05d6f9a0f6c930cbf749f
girishdev s <girishdev66@gmail.com>
2026-03-04 14:58:49 +0530

    Create Count Departments Already Sorted.C

---
+++ basic c/2D ARRAY/Count Departments Already Sorted.C
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+    int r,c,i,j,a[100][100],count=0;
+
+    scanf("%d %d",&r,&c);
+
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+            scanf("%d",&a[i][j]);
+
+        int sorted=1;
+
+        for(j=0;j<c-1;j++)
+        {
+            if(a[i][j]>a[i][j+1])
+                sorted=0;
+        }
+
+        if(sorted)
+            count++;
+    }
+
+    printf("%d",count);
+
+    return 0;
+}
